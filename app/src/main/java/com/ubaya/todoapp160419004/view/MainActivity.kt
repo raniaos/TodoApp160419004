c8e5e295275bbd03555f9573da967dd37bd8d153
rania ori sumargo <raniaos2001@gmail.com>
2022-04-13 10:47:45 +0700

    init 2

--- app/src/main/java/com/ubaya/todoapp160419004/view/MainActivity.kt
+++ app/src/main/java/com/ubaya/todoapp160419004/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ubaya.todoapp160419004
+package com.ubaya.todoapp160419004.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ubaya.todoapp160419004.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
